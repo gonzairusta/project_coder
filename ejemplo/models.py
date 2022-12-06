f2548b76f5c4ed129469f39b80ff3ca95a171226
gonzairusta <laloirusta@gmail.com>
2022-12-05 22:28:47 -0300

    mi segundo commit

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    nacimiento = models.CharField(max_length=10)
+    def __str__(self):
+      return f"{self.nombre}, {self.nacimiento}, {self.id}"
---
+++ seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="Gonzalo", direccion="Duarte Quiros 651", nacimiento="24/12/1997").save()
+Familiar(nombre="Cristina", direccion="Chacabuco 1304", nacimiento="07/10/1950").save()
+Familiar(nombre="Denise", direccion="Figueroa Alcorta 285", nacimiento="10/07/1998").save()
+
+print("Se cargo con éxito los usuarios de pruebas")
