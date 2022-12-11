5cc739232ba1acf561aa6a41918db6ed6f2b628a
gonzairusta <laloirusta@gmail.com>
2022-12-10 22:48:28 -0300

    modificaciones y correcciones

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Familiar(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
-    nacimiento = models.CharField(max_length=10)
+    nacimiento = models.DateField()
     def __str__(self):
       return f"{self.nombre}, {self.nacimiento}, {self.id}"
--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,6 @@
 from ejemplo.models import Familiar
-Familiar(nombre="Gonzalo", direccion="Duarte Quiros 651", nacimiento="24/12/1997").save()
-Familiar(nombre="Cristina", direccion="Chacabuco 1304", nacimiento="07/10/1950").save()
-Familiar(nombre="Denise", direccion="Figueroa Alcorta 285", nacimiento="10/07/1998").save()
+Familiar(nombre="Gonzalo", direccion="Duarte Quiros 651", nacimiento="1997-12-24").save()
+Familiar(nombre="Cristina", direccion="Chacabuco 1304", nacimiento="1950-10-07").save()
+Familiar(nombre="Denise", direccion="Figueroa Alcorta 285", nacimiento="1998-07-10").save()
 
 print("Se cargo con éxito los usuarios de pruebas")
